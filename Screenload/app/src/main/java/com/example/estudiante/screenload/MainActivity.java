0df36b9cc9e430aab29f86406b0989a900672cf5
majocabr <majocabr.93@gmail.com>
2016-06-26 02:10:46 -0500

    PrimerasPantallas

---
+++ Screenload/app/src/main/java/com/example/estudiante/screenload/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.estudiante.screenload;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final int DURACION_SPLASH = 10000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(MainActivity.this, Second.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
---
+++ Screenload/app/src/main/java/com/example/estudiante/screenload/Second.java
@@ -0,0 +1,15 @@
+package com.example.estudiante.screenload;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by estudiante on 23/06/16.
+ */
+public class Second extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otrapantalla);
+    }
+}
